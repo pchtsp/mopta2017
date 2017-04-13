8db10dc4fc1dabaa75b5231a0b435a85c50d3a17
Franco Peschiera <franco.peschiera@baobabsoluciones.es>
2017-04-13 20:16:58 +0200

    more calls to functions on main app.

--- python/main.py
+++ python/main.py
@@ -7,6 +7,15 @@ data_in = data_import.get_data_clean(data_dir)
 solution = heuristics.initial_solution(data_in)
 sol_costs = tests.get_costs(solution, data_in)
 
+tests.check_solution(solution, data_in)
+uncovered = tests.patients_not_covered(solution, data_in)
+
+tests.get_job_capacity(solution, data_in, True)
+
+job_characteristics = {job: data_in['production'][job_type] for job, job_type in solution['jobs_type'].items()}
+
+{job: data_in['production'][job_type] for job, job_type in solution['jobs_type'].items()}
+
 # sum(sol_costs.values())
 # data_in['costs']
 
